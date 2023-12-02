918c6b59f22c0dfa7df940c40afe138123ea9af6
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-02 21:00:00 +0500

    Exported Carousel component assets

---
+++ public/index.ts
@@ -0,0 +1,8 @@
+export { default as pig } from './pig.jpeg';
+export { default as carOne } from './carOne.jpeg';
+export { default as flying } from './flying.jpeg';
+export { default as glass } from './glass.png';
+export { default as decimal } from './decimal.jpg';
+export { default as funny } from './funny.jpg';
+export { default as maven } from './maven.jpg';
+export { default as powell } from './powell.jpg';
