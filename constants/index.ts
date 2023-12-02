9780965e53e4c2c03ae6dc719402ba9d80f4b29d
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-02 20:59:34 +0500

    Exported Carousel component constant

---
+++ constants/index.ts
@@ -0,0 +1,47 @@
+import { carOne, decimal, flying, funny, glass, maven, pig, powell } from "@public";
+
+export const servicesIcons = [
+    {
+        id: 1,
+        img: glass,
+        color: "#e3e5e7"
+    },
+    {
+        id: 2,
+        img: flying,
+        color: "#d6d7dc"
+    },
+    {
+        id: 3,
+        img: pig,
+        color: "#e3e3e3"
+    },
+    {
+        id: 4,
+        img: carOne,
+        color: "#21242b"
+    },
+];
+
+export const servicesIcons2 = [
+    {
+        id: 1,
+        img: maven,
+        color: "#d4e3ec"
+    },
+    {
+        id: 2,
+        img: powell,
+        color: "#e5e0e1"
+    },
+    {
+        id: 3,
+        img: funny,
+        color: "#d7d4cf"
+    },
+    {
+        id: 4,
+        img: decimal,
+        color: "#e1dad6"
+    },
+];
